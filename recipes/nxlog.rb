291b370c460e593812a081093cf4bb8e048fc6ed
daragh martin <d.martin@base2services.com>
2015-09-13 18:09:02 +1000

    added nxlog

--- recipes/nxlog.rb
+++ recipes/nxlog.rb
@@ -1,2 +1,16 @@
-include_recipe "nxlog"
+#use the cookbook to get the lates
+#we will control the template after that
+if !::File.file?('/usr/bin/nxlog')
+  include_recipe "nxlog"
+end
+
+template "/etc/nxlog/nxlog.conf" do
+  notifies :restart, "service[nxlog]"
+end
+
+service "nxlog" do
+  action [:enable, :start]
+  supports :status => true, :restart => true, :reload => true
+end
+
 
