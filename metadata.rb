8857e5918fc8dc777df76eb4ca2efede4b57d91a
Nikola Pajtic <npajtic@Nikolas-MacBook-Air.local>
2016-01-08 11:32:24 +1100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Instalfast-elk/Configures fast-elk'
 long_description 'Instalfast-elk/Configures fast-elk'
-version '0.1.2'
+version '0.1.3'
 
 depends 'nginx'
 depends 'java'
