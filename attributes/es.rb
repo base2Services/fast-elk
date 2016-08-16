67c5cf5203a10bbca8432a3fc0cd06a315a49597
Nikola Pajtic <npajtic@Nikolas-MacBook-Air.local>
2016-08-16 13:55:01 +1000

    region and tag value in elasticsearch.yml using node attributes

--- attributes/es.rb
+++ attributes/es.rb
@@ -1,3 +1,5 @@
 default['fast-elk']['es']['data_path'] = '/data/elasticsearch'
-default['fast-elk']['es']['ec2_tag'] = 'Environment'
+default['fast-elk']['es']['ec2_tag_name'] = 'Role'
+default['fast-elk']['es']['ec2_tag_value'] = "search"
 default['fast-elk']['es']['heapsize'] = '2g'
+default['fast-elk']['es']['region'] = 'ap-southeast-2'
